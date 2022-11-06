dbb33307a68da4164565436b8cb64ac9a34c04f2
Tai Danh <taid100@gmail.com>
2022-11-06 14:56:49 -0800

    save point, vars not tokenizing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use lalrpop_util::lalrpop_mod;
+use regex::Regex;
 
 lalrpop_mod!(pub rex);
 pub mod ast;
@@ -9,12 +10,16 @@ fn main() {
 }
 
 fn rex() {
+    let re = Regex::new(r"\$([[:alnum:]]+)").unwrap();
+    println!("re match: {}", re.is_match("$a1"));
+    let expr = rex::rexStmtParser::new().parse(r#"let $a1 match a1;"#).unwrap();
+    println!("{expr}");
     let expr = rex::rexStmtParser::new()
-        .parse(r#"create rz or z and y repeat;"#)
+        .parse(r#"create x or z and y repeat;"#)
         .unwrap();
     println!("{expr}");
     let expr = rex::rexStmtParser::new()
-        .parse(r#"let a1 = rz or z and (y repeat);"#)
+        .parse(r#"create rz or $a1 and y repeat;"#)
         .unwrap();
     println!("{expr}");
 }
