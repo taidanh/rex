fbc936a2e6d946ff6f67836cdd13cf458f65349a
Tai Danh <taid100@gmail.com>
2022-11-30 17:07:17 -0800

    starts adding cli options

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
+use std::path::Path;
+use std::fs;
+
+use clap::{Parser, ArgAction};
 use lalrpop_util::lalrpop_mod;
 // use regex::Regex;
 
 lalrpop_mod!(pub rex);
 pub mod ast;
 
+#[derive(Parser)]
+#[command(about)]
+struct Args {
+    #[arg(group = "input")]
+    input_file: Option<String>,
+
+    #[arg(short,long)]
+    /// Input your Rex program
+    rex: String,
+
+    #[arg(long,action = ArgAction::SetTrue)]
+    /// Output regex instead of matching on input
+    regex: bool,
+}
+
 fn main() {
-    println!("main");
-    rex();
+    let args = Args::parse();
+
+    let input = args.input_file.unwrap();
+    let file = Path::new(&input);
+    let rex_file = Path::new(&args.rex);
+
+    // println!("input: {:?}", file.is_file());
+    // println!("main");
+    // rex();
+    if file.is_file() {
+        let contents = fs::read_to_string(input);
+        let rex_match = rex::rexStmtParser::new().parse(contents);
+    } else {
+        let rex_match = rex::rexStmtParser::new().parse(&input);
+    }
 }
 
 fn rex() {
