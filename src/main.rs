5c281b00e0bf4c0b33c394d36113af83d8c98c1b
Tai Danh <taid100@gmail.com>
2022-12-08 15:30:34 -0800

    Updates README and grammar

--- src/main.rs
+++ src/main.rs
@@ -45,16 +45,16 @@ fn rex() {
     // let re = Regex::new(r"[.[^ ]]+").unwrap();
     // println!("z matches .: '{:?}'", re.find("1203!@)(#  b").unwrap());
     let expr = rex::rexStmtParser::new()
-        .parse(r#"create rz or z and rz repeat;"#)
+        .parse(r#"match rz or z and rz repeat;"#)
         .unwrap();
     let sm = expr.build_state_machine();
     println!("sm: {:#?}", sm);
     let rex = sm.rex_match("zrzrz".to_string());
     println!("matches 'zrzrz': {:#?}", rex);
-    let expr = rex::rexStmtParser::new()
-        .parse(r#"create rz or z and y repeat;"#)
-        .unwrap();
-    println!("matches 'zyyy': {:#?}", expr.build_state_machine().rex_match("zyyy".to_string()));
+    // let expr = rex::rexStmtParser::new()
+    //     .parse(r#"create rz or z and y repeat;"#)
+    //     .unwrap();
+    // println!("matches 'zyyy': {:#?}", expr.build_state_machine().rex_match("zyyy".to_string()));
     // let expr = rex::rexStmtParser::new()
     //     .parse(r#"let $a1 = rz or z and (y repeat);"#)
     //     .unwrap();
